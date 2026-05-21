9b1c811a69088aff694fba6fe466b8db85f4cbaf
stephane martin <stephane@s-martin.fr>
2026-05-21 16:15:53 +0200

    first front

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,29 @@
 Rails.application.routes.draw do
+  get "bookmarks/new"
+  get "bookmarks/create"
+  get "bookmarks/destroy"
+  get "lists/index"
+  get "lists/show"
+  get "lists/new"
+  get "lists/create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
+
+  # config/routes.rb
+
+
+  root to: "lists#index"
+
+  resources :lists, only: [ :index, :show, :new, :create ] do
+    resources :bookmarks, only: [ :new, :create ]
+  end
+
+  resources :bookmarks, only: [ :destroy ]
+
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
