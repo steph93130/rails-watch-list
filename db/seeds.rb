58798fc69436868541aa52b553359c1c1663f1f5
stephane martin <stephane@s-martin.fr>
2026-05-21 12:59:13 +0200

    Movies created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,28 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# db/seeds.rb
+
+require "open-uri"
+require "json"
+
+puts "Cleaning database..."
+
+Bookmark.destroy_all
+List.destroy_all
+Movie.destroy_all
+
+puts "Fetching top rated movies from Le Wagon TMDB proxy..."
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+
+serialized_movies = URI.open(url).read
+movies = JSON.parse(serialized_movies)
+
+movies["results"].each do |movie|
+  Movie.create!(
+    title: movie["title"],
+    overview: movie["overview"],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie["poster_path"]}",
+    rating: movie["vote_average"]
+  )
+end
+
+puts "#{Movie.count} movies created!"
